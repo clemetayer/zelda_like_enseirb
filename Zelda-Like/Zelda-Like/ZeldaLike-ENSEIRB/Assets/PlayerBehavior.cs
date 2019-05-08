62912589288630e04a838b3e169cfa5a8a67514a
waterproof <waterproof56@gmail.com>
2019-05-08 10:42:10 +0200

    truc à revert

--- Zelda-Like/Zelda-Like/ZeldaLike-ENSEIRB/Assets/PlayerBehavior.cs
+++ Zelda-Like/Zelda-Like/ZeldaLike-ENSEIRB/Assets/PlayerBehavior.cs
@@ -10,59 +10,24 @@ using UnityEngine;
 
 // Represents the cardinal directions (South, North, West, East)
 public enum CardinalDirections { CARDINAL_S, CARDINAL_N, CARDINAL_W, CARDINAL_E };
-public enum HeroColor {NONE, RED, GREEN, BLUE}
 
 public class PlayerBehavior : MonoBehaviour
 {
     public float m_speed = 1f; // Speed of the player when he moves
     private CardinalDirections m_direction; // Current facing direction of the player
-	private HeroColor h_color = HeroColor.NONE; // Sets the base hero color to NONE
 
     public Sprite m_frontSprite = null;
     public Sprite m_leftSprite = null;
     public Sprite m_rightSprite = null;
     public Sprite m_backSprite = null;
 
-	//red player
-	public Sprite m_REDfrontSprite = null; 
-    public Sprite m_REDleftSprite = null;
-    public Sprite m_REDrightSprite = null;
-    public Sprite m_REDbackSprite = null;
-	
-	//green player
-	public Sprite m_GREENfrontSprite = null;
-    public Sprite m_GREENleftSprite = null;
-    public Sprite m_GREENrightSprite = null;
-    public Sprite m_GREENbackSprite = null;
-	
-	//blue player
-	public Sprite m_BLUEfrontSprite = null;
-    public Sprite m_BLUEleftSprite = null;
-    public Sprite m_BLUErightSprite = null;
-    public Sprite m_BLUEbackSprite = null;
-	
     public GameObject m_fireBall = null; // Object the player can shoot
-	
-	public GameObject m_REDfrontFireBall = null; // Object the player can shoot (red fireball)
-	public GameObject m_REDleftFireBall = null; // Object the player can shoot (red fireball)
-	public GameObject m_REDrightFireBall = null; // Object the player can shoot (red fireball)
-	public GameObject m_REDbackFireBall = null; // Object the player can shoot (red fireball)
-	
-	public GameObject m_GREENfrontFireBall = null; // Object the player can shoot (green fireball)
-	public GameObject m_GREENleftFireBall = null; // Object the player can shoot (green fireball)
-	public GameObject m_GREENrightFireBall = null; // Object the player can shoot (green fireball)
-	public GameObject m_GREENbackFireBall = null; // Object the player can shoot (green fireball)
-	
-	public GameObject m_BLUEfrontFireBall = null; // Object the player can shoot (blue fireball)
-	public GameObject m_BLUEleftFireBall = null; // Object the player can shoot (blue fireball)
-	public GameObject m_BLUErightFireBall = null; // Object the player can shoot (blue fireball)
-	public GameObject m_BLUEbackFireBall = null; // Object the player can shoot (blue fireball)
 
     public GameObject m_map = null;
     public DialogManager m_dialogDisplayer;
 
     private Dialog m_closestNPCDialog;
-	
+
     Rigidbody2D m_rb2D;
     SpriteRenderer m_renderer;
 
@@ -151,6 +116,7 @@ public class PlayerBehavior : MonoBehaviour
         {
             return;
         }
+
         ChangeSpriteToMatchDirection();
 
         // If the player presses SPACE, then two solution
@@ -186,284 +152,60 @@ public class PlayerBehavior : MonoBehaviour
         {
             ShootFireball('d');
         }
-		if ((Input.GetKeyDown(KeyCode.Keypad1)) && (h_color == HeroColor.RED))
-        {
-            h_color = HeroColor.NONE;
-        }
-		else if (Input.GetKeyDown(KeyCode.Keypad1))
-        {
-            h_color = HeroColor.RED;
-        }
-		if ((Input.GetKeyDown(KeyCode.Keypad2)) && (h_color == HeroColor.GREEN))
-        {
-            h_color = HeroColor.NONE;
-        }
-		else if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            h_color = HeroColor.GREEN;
-        }
-		if ((Input.GetKeyDown(KeyCode.Keypad3)) && (h_color == HeroColor.BLUE))
-        {
-            h_color = HeroColor.NONE;
-        }
-		else if (Input.GetKeyDown(KeyCode.Keypad3))
-        {
-            h_color = HeroColor.BLUE;
-        }
     }
 
     // Changes the player sprite regarding it position
     // (back when going North, front when going south, right when going east, left when going west)
     private void ChangeSpriteToMatchDirection()
     {
-		if(h_color == HeroColor.NONE)
-		{
-			if (m_direction == CardinalDirections.CARDINAL_N)
-			{
-				m_renderer.sprite = m_backSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_S)
-			{
-				m_renderer.sprite = m_frontSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_E)
-			{
-				m_renderer.sprite = m_rightSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_W)
-			{
-				m_renderer.sprite = m_leftSprite;
-			}
-		}
-		if(h_color == HeroColor.RED)
-		{
-			if (m_direction == CardinalDirections.CARDINAL_N)
-			{
-				m_renderer.sprite = m_REDbackSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_S)
-			{
-				m_renderer.sprite = m_REDfrontSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_E)
-			{
-				m_renderer.sprite = m_REDrightSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_W)
-			{
-				m_renderer.sprite = m_REDleftSprite;
-			}
-		}
-		if(h_color == HeroColor.GREEN)
-		{
-			if (m_direction == CardinalDirections.CARDINAL_N)
-			{
-				m_renderer.sprite = m_GREENbackSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_S)
-			{
-				m_renderer.sprite = m_GREENfrontSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_E)
-			{
-				m_renderer.sprite = m_GREENrightSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_W)
-			{
-				m_renderer.sprite = m_GREENleftSprite;
-			}
-		}
-		if(h_color == HeroColor.BLUE)
-		{
-			if (m_direction == CardinalDirections.CARDINAL_N)
-			{
-				m_renderer.sprite = m_BLUEbackSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_S)
-			{
-				m_renderer.sprite = m_BLUEfrontSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_E)
-			{
-				m_renderer.sprite = m_BLUErightSprite;
-			}
-			else if (m_direction == CardinalDirections.CARDINAL_W)
-			{
-				m_renderer.sprite = m_BLUEleftSprite;
-			}
-		}
+        if (m_direction == CardinalDirections.CARDINAL_N)
+        {
+            m_renderer.sprite = m_backSprite;
+        }
+        else if (m_direction == CardinalDirections.CARDINAL_S)
+        {
+            m_renderer.sprite = m_frontSprite;
+        }
+        else if (m_direction == CardinalDirections.CARDINAL_E)
+        {
+            m_renderer.sprite = m_rightSprite;
+        }
+        else if (m_direction == CardinalDirections.CARDINAL_W)
+        {
+            m_renderer.sprite = m_leftSprite;
+        }
     }
 
     // Creates a fireball, and launches it
     private void ShootFireball(char key)
     {
-		if(h_color == HeroColor.NONE)
-		{
-			GameObject newFireball = Instantiate(m_fireBall, this.transform) as GameObject;
+        GameObject newFireball = Instantiate(m_fireBall, this.transform) as GameObject;
 
-			FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
+        FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
 
-			if (fireBallBehavior != null)
-			{
-				if (key == 'z')
-				{
-					fireBallBehavior.Launch(new Vector2(0f, 1f));
-				}
-				if (key == 's')
-				{
-					fireBallBehavior.Launch(new Vector2(0f, -1f));
-				}
-				if (key == 'd')
-				{
-					fireBallBehavior.Launch(new Vector2(1f, 0f));
-				}
-				if (key == 'q')
-				{
-					fireBallBehavior.Launch(new Vector2(-1f, 0f));
-				}
-			}
-		}
-		else if(h_color == HeroColor.RED)
-		{
+        if (fireBallBehavior != null)
+        {
 			if (key == 'z')
 			{
-				GameObject newFireball = Instantiate(m_REDfrontFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(0f, 1f));
-				}
+				fireBallBehavior.Launch(new Vector2(0f, 1f));
 			}
 			if (key == 's')
 			{
-				GameObject newFireball = Instantiate(m_REDbackFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(0f, -1f));
-				}
+				fireBallBehavior.Launch(new Vector2(0f, -1f));
 			}
 			if (key == 'd')
 			{
-				GameObject newFireball = Instantiate(m_REDrightFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(1f, 0f));
-				}
+				fireBallBehavior.Launch(new Vector2(1f, 0f));
 			}
 			if (key == 'q')
 			{
-				GameObject newFireball = Instantiate(m_REDleftFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(-1f, 0f));
-				}
+				fireBallBehavior.Launch(new Vector2(-1f, 0f));
 			}
-		}
-		if(h_color == HeroColor.GREEN)
-		{
-			if (key == 'z')
-			{
-				GameObject newFireball = Instantiate(m_GREENfrontFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(0f, 1f));
-				}
-			}
-			if (key == 's')
-			{
-				GameObject newFireball = Instantiate(m_GREENbackFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(0f, -1f));
-				}
-			}
-			if (key == 'd')
-			{
-				GameObject newFireball = Instantiate(m_GREENrightFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(1f, 0f));
-				}
-			}
-			if (key == 'q')
-			{
-				GameObject newFireball = Instantiate(m_GREENleftFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(-1f, 0f));
-				}
-			}
-		}
-		if(h_color == HeroColor.BLUE)
-		{
-			if (key == 'z')
-			{
-				GameObject newFireball = Instantiate(m_BLUEfrontFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(0f, 1f));
-				}
-			}
-			if (key == 's')
-			{
-				GameObject newFireball = Instantiate(m_BLUEbackFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(0f, -1f));
-				}			
-			}
-			if (key == 'd')
-			{
-				GameObject newFireball = Instantiate(m_BLUErightFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(1f, 0f));
-				}			
-			}
-			if (key == 'q')
-			{
-				GameObject newFireball = Instantiate(m_BLUEleftFireBall, this.transform) as GameObject;
-			
-				FireBehavior fireBallBehavior = newFireball.GetComponent<FireBehavior>();
-
-				if (fireBallBehavior != null)
-				{
-					fireBallBehavior.Launch(new Vector2(-1f, 0f));
-				}
-			}
-		}
+            // Lauches the fireball upward
+            // (Vector2 represents a direction in x and y ;
+            // so Vector2(0f, 1f) is a direction of 0 in x and 1 in y (up)
+            //fireBallBehavior.Launch(new Vector2(0f, 1f));
+        }
     }
 
 
